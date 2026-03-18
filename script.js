f8360050e6e5fa0699791ec9215520ebb287356f
Guilherme França <guilfranca27@gmail.com>
2026-03-17 23:49:49 -0300

    implement mobile header

---
+++ script.js
@@ -0,0 +1,11 @@
+// animation function header
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('.header');
+    if(window.scrollY > 50) {
+        header.style.background = '#111';
+        header.style.padding = '15px 0';
+    } else {
+        header.style.background = 'rgba(0, 0, 0, 0.9)';
+        header.style.padding = '20px 0';
+    }
+});
