a6f3805a59c22bd5d83968c3cd2bc0ce41647b79
Guilherme França <guilfranca27@gmail.com>
2026-03-18 12:50:36 -0300

    main - hero complet

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ window.addEventListener('scroll', function() {
     const header = document.querySelector('.header');
     if(window.scrollY > 50) {
         header.style.background = '#111';
-        header.style.padding = '15px 0';
+        header.style.padding = '10px 0';
     } else {
         header.style.background = 'rgba(0, 0, 0, 0.9)';
-        header.style.padding = '20px 0';
+        header.style.padding = '15px 0';
     }
 });
