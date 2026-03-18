857b22e08291ae473c962cdeae270aca57ab7e61
Guilherme França <guilfranca27@gmail.com>
2026-03-18 14:35:21 -0300

    modalidades section responsive completed

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 window.addEventListener('scroll', function() {
     const header = document.querySelector('.header');
     if(window.scrollY > 50) {
-        header.style.background = '#111';
+        header.style.background = 'rgba(0, 0, 0, 0.9)';
         header.style.padding = '10px 0';
     } else {
-        header.style.background = 'rgba(0, 0, 0, 0.9)';
+        header.style.background = '#000';
         header.style.padding = '15px 0';
     }
 });
