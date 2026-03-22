af2e88ae89eb9f51963e377ef99148a13cbd97a5
Guilherme França <guilfranca27@gmail.com>
2026-03-22 09:26:14 -0300

    fix header mobile

--- script.js
+++ script.js
@@ -14,6 +14,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
     window.addEventListener('scroll', function () {
         const header = document.querySelector('.header');
+        if (window.innerWidth < 768) {
+            return
+        }
         if (window.scrollY > 50) {
             header.style.background = 'rgba(0, 0, 0, 0.9)';
             header.style.padding = '10px 0';
